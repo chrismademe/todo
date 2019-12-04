510f293ec4569f35919b3c79e5ceba28c5cfe5fa
Chris <chris@underscores.co.uk>
2019-12-04 21:49:00 +0000

    Comment code better

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 (() => {
     window.taskList = []
 
+    /**
+     * Handle Key Press
+     * @param {*} e Key press event
+     * 
+     * Handles creating new tasks and deleting tasks when they're empty
+     */
     const handleKeyPress = (e) => {
         saveTasks()
 
@@ -15,14 +21,26 @@
         }
     }
 
+    /**
+     * Handle Check Press
+     * @param {*} e Event
+     * 
+     * Updates a task when it you check the checkbox
+     */
     const handleCheckPress = (e) => {
         updateTask(e.target.parentNode, e.target.checked)
         saveTasks()
     }
 
+    /**
+     * Update Task
+     * @param string task 
+     * @param bool isDone 
+     * 
+     * Updates task state
+     */
     const updateTask = (task, isDone) => {
         let input = task.querySelector('input[type="text"]')
-        let checkbox = task.querySelector('input[type="checkbox"]')
         
         if ( isDone ) {
             input.classList.add('line-through', 'text-gray-400')
@@ -36,12 +54,23 @@
         saveTasks()
     }
 
+    /**
+     * Remove Task
+     * @param HTMLElement task 
+     * 
+     * Removes a task from the list
+     */
     const removeTask = (task) => {
         task.previousElementSibling.querySelector('input[type="text"]').focus()
         task.parentNode.removeChild(task)
         saveTasks()
     }
 
+    /**
+     * Save Tasks
+     * 
+     * Saves the task list to localStorage
+     */
     const saveTasks = () => {
         let tasks = document.querySelectorAll('[data-task]')
         let tasksToSave = []
@@ -61,43 +90,57 @@
         localStorage.setItem('tasks', JSON.stringify(tasksToSave))
     }
 
+    /**
+     * Create Task
+     * @param string name 
+     * @param bool isDone
+     * 
+     * Creates a new task 
+     */
     const createTask = (name = null, isDone = false) => {
         let taskList = document.querySelector('.list')
 
+        // Create Task Element
         let task = document.createElement('li')
         task.setAttribute('data-task', 1)
         task.classList.add('flex', 'bg-white', 'mt-1', 'text-gray-800')
 
+        // Create Task Checkbox
         let checkbox = document.createElement('input')
         checkbox.setAttribute('type', 'checkbox')
         checkbox.classList.add('p-2', 'cursor-pointer')
         checkbox.style.transform = 'scale(1.1)'
         checkbox.addEventListener('click', handleCheckPress)
 
-        if ( status === true ) {
+        // Update checkbox state if isDone
+        if ( isDone ) {
             checkbox.setAttribute('checked', true)
         }
 
+        // Create Task Input
         let input = document.createElement('input')
         input.setAttribute('type', 'text')
         input.classList.add('ml-2')
         input.setAttribute('placeholder', 'What do you need to do?')
         input.addEventListener('keyup', handleKeyPress)
 
+        // Populate the name field if set
         if ( name ) {
             input.value = name
         }
 
+        // Add the task to the list
         task.appendChild(checkbox)
         task.appendChild(input)
-
         taskList.appendChild(task)
 
+        // Focus on the new task
         input.focus()
 
         saveTasks()
     }
 
+    // Get stored tasks
     const tasks = localStorage.getItem('tasks')
 
     if ( tasks ) {
@@ -110,6 +153,7 @@
         createTask()
     }
 
+    // Handle click on Add Task button
     const addTaskBtn = document.querySelector('[data-action="new-task"]')
     addTaskBtn.addEventListener('click', (e) => {
         e.preventDefault()
